7f485de24d5a56c34cbde7abf4c49e7fe50a0784
Luiz Fellipe <luiz.fellipe@scandiweb.com>
2024-02-04 20:44:06 -0300

    fix flash msg

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('message'))
- <div x-data='{show: true}'
+ <div x-data='{show: true}' x-init="setTimeout(() => show = false, 3000)"
      x-show='show' class=" z-10 fixed whitespace-nowrap top-20 shadow-md rounded-xl left-1/2 transform -translate-x-1/2 bg-sky-400 text-white px-48 py-3">
     <p>
         {{session('message')}}
